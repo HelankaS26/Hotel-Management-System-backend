faa39b76e97ed27862b876e21922b7ddd2db28a0
Helanka Singhapurage <helankas26@gmail.com>
2022-10-08 11:38:33 +0530

    generate JPA Entities from DB

---
+++ src/main/java/com/hotelmanagement/entity/Bonus.java
@@ -0,0 +1,101 @@
+package com.hotelmanagement.entity;
+
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "bonuses", indexes = {
+        @Index(name = "employeeID", columnList = "employeeID")
+})
+public class Bonus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bonusID", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "employeeID", nullable = false)
+    private Employee employeeID;
+
+    @NotNull
+    @Column(name = "amount", nullable = false, precision = 7, scale = 2)
+    private BigDecimal amount;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "description", nullable = false, length = 100)
+    private String description;
+
+    @NotNull
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Employee getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(Employee employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Bonus bonus = (Bonus) o;
+        return id != null && Objects.equals(id, bonus.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "amount = " + amount + ", " +
+                "description = " + description + ", " +
+                "date = " + date + ")";
+    }
+}
