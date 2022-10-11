6cb4566c6099d61cacfc99063fa560582d4122ff
Helanka Singhapurage <helankas26@gmail.com>
2022-10-11 06:03:09 +0530

    intergrate first procedure

--- src/main/java/com/hotelmanagement/controller/EmployeeController.java
+++ src/main/java/com/hotelmanagement/controller/EmployeeController.java
@@ -1,9 +1,44 @@
 package com.hotelmanagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hotelmanagement.domain.ResourceEmployeeDTO;
+import com.hotelmanagement.domain.StoreEmployeeDTO;
+import com.hotelmanagement.domain.UpdateEmployeeDTO;
+import com.hotelmanagement.service.EmployeeServiceable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("api/v1.0/employees")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeServiceable employeeServiceable;
+
+    @GetMapping()
+    public List<ResourceEmployeeDTO> index() {
+        return employeeServiceable.getAllEmployees();
+    }
+
+    @PostMapping()
+    public ResponseEntity<Boolean> store(@RequestBody StoreEmployeeDTO storeEmployeeDTO) {
+        return employeeServiceable.createEmployee(storeEmployeeDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResourceEmployeeDTO show(@PathVariable String id) {
+        return employeeServiceable.getEmployeeByID(id);
+    }
+
+    @PutMapping("/{id}")
+    public Object update(@RequestBody UpdateEmployeeDTO updateEmployeeDTO) {
+        return employeeServiceable.updateEmployee(updateEmployeeDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public String destroy(@PathVariable String id) {
+        return employeeServiceable.deleteEmployee(id);
+    }
 }
--- src/main/java/com/hotelmanagement/service/EmployeeServiceable.java
+++ src/main/java/com/hotelmanagement/service/EmployeeServiceable.java
@@ -1,4 +1,20 @@
 package com.hotelmanagement.service;
 
+import com.hotelmanagement.domain.ResourceEmployeeDTO;
+import com.hotelmanagement.domain.StoreEmployeeDTO;
+import com.hotelmanagement.domain.UpdateEmployeeDTO;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface EmployeeServiceable {
+    List<ResourceEmployeeDTO> getAllEmployees();
+
+    ResponseEntity<Boolean> createEmployee(StoreEmployeeDTO storeEmployeeDTO);
+
+    ResourceEmployeeDTO getEmployeeByID(String id);
+
+    Object updateEmployee(UpdateEmployeeDTO updateEmployeeDTO);
+
+    String deleteEmployee(String id);
 }
