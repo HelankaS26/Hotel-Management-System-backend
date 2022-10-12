3f948cbe137a1acf60bf146ba107da04cfd159ec
Helanka Singhapurage <helankas26@gmail.com>
2022-10-12 15:26:59 +0530

    add new Spring Data JPA Repository

--- src/main/java/com/hotelmanagement/repository/UserloginrecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hotelmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserloginrecordRepository extends JpaRepository<Userloginrecord, UserloginrecordId> {
-}
