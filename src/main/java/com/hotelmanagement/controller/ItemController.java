2a0c57547f653bd97a5a324bb68d19ab1e78d272
Helanka Singhapurage <helankas26@gmail.com>
2022-10-13 10:19:44 +0530

    implement Item APIs

--- src/main/java/com/hotelmanagement/controller/ItemController.java
+++ src/main/java/com/hotelmanagement/controller/ItemController.java
@@ -1,9 +1,45 @@
 package com.hotelmanagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hotelmanagement.dto.ResourceItemDTO;
+import com.hotelmanagement.dto.StoreItemDTO;
+import com.hotelmanagement.dto.UpdateItemDTO;
+import com.hotelmanagement.service.ItemServiceable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/items")
+@RequestMapping("api/v1.0/items")
 public class ItemController {
+
+    @Autowired
+    private ItemServiceable itemServiceable;
+
+    @GetMapping()
+    public ResponseEntity<List<ResourceItemDTO>> index() {
+        return ResponseEntity.ok(itemServiceable.getAllItems());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Boolean> store(@RequestBody StoreItemDTO storeItemDTO) {
+        return itemServiceable.createItem(storeItemDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResourceItemDTO> show(@PathVariable String id) {
+        return ResponseEntity.ok(itemServiceable.getItemByID(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Boolean> update(@PathVariable String id, @RequestBody UpdateItemDTO updateItemDTO) {
+        updateItemDTO.setId(id);
+        return itemServiceable.updateItem(updateItemDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> destroy(@PathVariable String id) {
+        return itemServiceable.deleteItem(id);
+    }
 }
