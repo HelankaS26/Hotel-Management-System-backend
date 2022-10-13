37c04e5de3e7ac8edbe2542df1aca546f79ed2e5
Helanka Singhapurage <helankas26@gmail.com>
2022-10-13 07:31:50 +0530

    implement Advance APIs

--- src/main/java/com/hotelmanagement/controller/AdvanceController.java
+++ src/main/java/com/hotelmanagement/controller/AdvanceController.java
@@ -1,9 +1,43 @@
 package com.hotelmanagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hotelmanagement.dto.*;
+import com.hotelmanagement.service.AdvanceServiceable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/advances")
+@RequestMapping("api/v1.0/advances")
 public class AdvanceController {
+    @Autowired
+    private AdvanceServiceable advanceServiceable;
+
+    @GetMapping()
+    public ResponseEntity<List<ResourceAdvanceDTO>> index() {
+        return ResponseEntity.ok(advanceServiceable.getAllAdvances());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Boolean> store(@RequestBody StoreAdvanceDTO storeAdvanceDTO) {
+        return advanceServiceable.createAdvance(storeAdvanceDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResourceAdvanceDTO> show(@PathVariable Integer id) {
+        return ResponseEntity.ok(advanceServiceable.getAdvanceByID(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Boolean> update(@PathVariable Integer id, @RequestBody UpdateAdvanceDTO updateAdvanceDTO) {
+        updateAdvanceDTO.setId(id);
+        return advanceServiceable.updateAdvance(updateAdvanceDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> destroy(@PathVariable Integer id) {
+        return advanceServiceable.deleteAdvance(id);
+    }
 }
